bd4246fd93b2c21d545a5380aafdbdf938e8ad10
Randy Llobera <randy.llobera@gmail.com>
2026-01-06 13:05:14 +0100

    Add Navbar and Hero components.

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,3 +1,4 @@
+import Hero from '~/components/Hero';
 import type { Route } from './+types/index';
 
 export function meta({}: Route.MetaArgs) {
@@ -8,5 +9,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <section>My App!</section>;
+  return (
+    <section>
+      <Hero name='Randy' />
+    </section>
+  );
 }
