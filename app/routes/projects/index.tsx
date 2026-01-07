fd729911838a2396328c116834bc0e9cd5dc8430
Randy Llobera <randy.llobera@gmail.com>
2026-01-07 11:47:32 +0100

    Add json-server dependency. Add data and images. Add loader to fetch projects using json-server.

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,20 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  console.log(projects);
+
   return (
     <>
       <h2 className='text-3xl font-bold text-white mb-8'>🚀 Projects</h2>
+      <p>{projects}</p>
     </>
   );
 };
