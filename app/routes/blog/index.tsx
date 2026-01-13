df7f78cc0b20f73cc00ab7e8171d2f25ba693f3e
Randy Llobera <randy.llobera@gmail.com>
2026-01-13 15:52:44 +0100

    Add markdown posts. Add post-metadata.json file.Add posts to blog page. Add new PostCard component to display lists of posts.

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,8 +1,29 @@
-const BlogPage = () => {
+import type { Route } from './+types';
+import type { PostMeta } from '~/types';
+import PostCard from '~/components/PostCard';
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL('/posts-meta.json', request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error('Failed to fetch data');
+  const data = await res.json();
+
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const posts: PostMeta[] = loaderData.posts;
+
   return (
-    <>
-      <h1 className='text-3xl font-bold text-white mb-8'>📝 Blog</h1>
-    </>
+    <div className='max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900'>
+      <h2 className='text-3xl text-white font-bold mb-8'>📝 Blog</h2>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
   );
 };
 
